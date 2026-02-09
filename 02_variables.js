0b3774f19a3a2c392ae4977e926b38f50eb7a194
Nirajkumar423 <nirajkumarsah423@gmail.com>
2026-02-09 19:26:55 +0530

    Data_types

---
+++ 02_variables.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+// alert("Hello rajaji");we are using node.js not broser
+// console.log(3 + 3);
+// console.log("Niraj Kumar");
+// alert()
+// alert(3 + 3)
+let name = "niraj"
+let age = 18
+let isloggedIn = false
+// int state = null
+
+console.log(typeof "niraj");
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null);
+
