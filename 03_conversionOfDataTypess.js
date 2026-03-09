bdf2fe90975a3a8375164f50fca50a5a1d070b1c
Nirajkumar423 <nirajkumarsah423@gmail.com>
2026-03-09 20:35:12 +0530

    operations

--- 03_conversionOfDataTypess.js
+++ 03_conversionOfDataTypess.js
@@ -1,13 +1,25 @@
 // let score = "33abc"
 // let score = null
 // let score = true
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-let isloggedIn=1
-console.log(isloggedIn);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let isloggedIn=1
+// console.log(isloggedIn);
+
+//     **************operations *************
+let value =3
+let negvalue=-value
+// console.log(negvalue)
 
 
+let str1="hello"
+let str2="Niraj"
+let str3=str1+ " "+ str2
+console.log(str3);
+console.log("1" +1 +1)
+console.log(+true)
+console.log(+"")
