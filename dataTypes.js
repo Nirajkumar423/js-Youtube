5c17c712f41c7be9c8350eb11de3e2d5aacb3c15
Nirajkumar423 <nirajkumarsah423@gmail.com>
2026-03-10 09:51:30 +0530

    comparision

---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+//premetive-7types
+//string,number,boolean,null,undefined,symbol,BigInt
+
+
+const score=100
+const scoreValue=100.3
+const isLogged=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol("123")
+console.log(id==anotherId)
+const bigNumber=123456978n
+
+//refrence types(non-premetive)
+//arrays,object,functions,
+
+const heros=["shaktiman","naagraj","doga"]
+{
+    name:"niraj ",
+    age=22
+}
+
+
+const myFunction=function(){
+    console.log("Hello world")
+}
