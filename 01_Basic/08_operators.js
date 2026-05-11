dffc28b74b78f4128f5a2506b77208954e2a1e03
Nirajkumar423 <nirajkumarsah423@gmail.com>
2026-05-11 15:07:31 +0530

    operators

---
+++ 01_Basic/08_operators.js
@@ -0,0 +1,24 @@
+//operator
+// arithmatic
+// logical 
+// comparision
+// assignment operator
+// Unary
+// ternary
+let x=10;
+let y=20;
+if(x>5 && y<25){
+    console.log("A");
+}
+else{
+    console.log("B");
+}
+
+
+let temp=35; 
+if(!(temp<30)){
+    console.log("Hot");
+}
+else{
+    console.log("plesent");
+}
---
+++ 02_Basic/01_array.js
@@ -0,0 +1,6 @@
+const myarr = [0, 1, 2, 3, 4, 5]
+const myheros = ["saktiman", "raju", "Ayush"]
+
+const myarr2 = new Array (1,2,3,4,5)
+
+console.log(myarr);
